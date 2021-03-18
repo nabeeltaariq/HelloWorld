c173de4a11bc6a0d64324a760e2374d65feef6e9
nabeeltaariq <nabeeltaariq@gmail.com>
2021-03-18 06:26:25 -0700

    Add two numbers and int.parse

--- Program.cs
+++ Program.cs
@@ -6,11 +6,24 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter your name:");
-            
-            string name = Console.ReadLine();
+            Console.WriteLine("Enter First Number: ");
+            int num= int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Second Number: ");
+            int num2 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Your name is  {0}" , name);
+            int sum = num + num2;
+
+            Console.WriteLine("Addition is {0}", sum);
+
+
+            //Console.WriteLine("Enter your Firstname:");
+            //string fname = Console.ReadLine();
+
+            //Console.WriteLine("Enter your Lastname:");
+            //string lname = Console.ReadLine();
+
+
+            //Console.WriteLine("Your name is  {0} {1}" , fname, lname);
         }
     }
 }
